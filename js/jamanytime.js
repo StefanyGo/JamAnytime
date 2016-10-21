a6f5838a289f3f4ed85478144244b479fedbdb08
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-10-21 17:24:44 -0400

    Added drumBtn w/ sound

---
+++ js/jamanytime.js
@@ -0,0 +1,11 @@
+// JamAnytime
+// Fall 2016 Ignite Project
+// @Stafany Go
+
+
+var drums = new Pizzicato.Sound('sounds/drums.m4a');
+
+$("#drumBtn").click(function() {
+	alert("Clicked!");
+	drums.play();
+});
